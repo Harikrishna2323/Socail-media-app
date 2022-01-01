fbb51d8e370322e00efbf4b4147c75507565e003
harikrishna2323 <harikb23@gmail.com>
2022-01-01 13:03:19 +0530

    path configurations

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use("/api/conversations", convRouter);
 app.use("/api/messages", messageRouter);
 
 if (process.env.NODE_ENV === "PRODUCTION") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.userRouter(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
